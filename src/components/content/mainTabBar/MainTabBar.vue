96963a387dfe1d1792affc1f164e1ad95f81ba57
ChanSing <273526292@qq.com>
2021-06-03 15:04:02 +0800

    first commit

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="" />
+        <img
+          slot="item-icon_active"
+          src="~assets/img/tabbar/home2.svg"
+          alt=""
+        />
+        <div slot="item-name">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="" />
+        <img
+          slot="item-icon_active"
+          src="~assets/img/tabbar/category2.svg"
+          alt=""
+        />
+        <div slot="item-name">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img slot="item-icon" src="~assets/img/tabbar/cart.svg" alt="" />
+        <img
+          slot="item-icon_active"
+          src="~assets/img/tabbar/cart2.svg"
+          alt=""
+        />
+        <div slot="item-name">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img slot="item-icon" src="~assets/img/tabbar/user.svg" alt="" />
+        <img
+          slot="item-icon_active"
+          src="~assets/img/tabbar/user2.svg"
+          alt=""
+        />
+        <div slot="item-name">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style scoped>
+</style>
